cf419346cecb080b8e0065e8ce872e5da2c8d2b5
Vishnu-Das-T-M <115615905+Vishnu-Das-T-M@users.noreply.github.com>
2024-02-16 15:29:47 +0530

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,6 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), 
-    importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"angularcrudfirebase-2ce84","appId":"1:662831298501:web:575d7c2243e82292d0fdac","storageBucket":"angularcrudfirebase-2ce84.appspot.com","apiKey":"AIzaSyC9HS2SiFQGt4BtBKnUo8Ek2ORuQt0YotU","authDomain":"angularcrudfirebase-2ce84.firebaseapp.com","messagingSenderId":"662831298501"}))), importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp({}))), importProvidersFrom(provideFirestore(() => getFirestore())),
   ]
 };
